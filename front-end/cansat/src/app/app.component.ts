4db5adfe212e80512fab0330720be5bdc4edf693
OrlandoRNX <orlandorivasrubio@live.com>
2020-11-09 23:38:07 -0500

    changes

--- front-end/cansat/src/app/app.component.ts
+++ front-end/cansat/src/app/app.component.ts
@@ -26,16 +26,17 @@ export class AppComponent {
   }
 
   load(): void {
+
     let dataPoints = [];
     let y = 0;
     for ( var i = 0; i < 10000; i++ ) {
+      let now = new Date().getDay() + i;
       y += Math.round(5 + Math.random() * (-5 - 5));
-      dataPoints.push({ y: y});
+      dataPoints.push({ y: y, x: now});
     }
     let chart = new CanvasJS.Chart("chartContainer", {
       zoomEnabled: true,
       animationEnabled: true,
-      exportEnabled: true,
       title: {
         text: "Vibración del dispositivo"
       },
